5ae7a70da7f576a48589d32064b41fdd15f18e5a
akgoode <akgoode@gmail.com>
2019-07-26 14:40:41 -0500

    https

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const http = require('http');
+const https = require('https');
 
 const PORT = process.env.PORT || 8080;
 
 require('./modules/router')(app);
-const server = http.createServer(app);
+const server = https.createServer(app);
 
 server.listen(PORT,() => {
 	console.log('Listening on port' + PORT);
